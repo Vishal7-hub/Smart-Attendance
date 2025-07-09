2bdd8c01c4f057e8391037d5741726bed8509ebe
Vishal7hub <vishal9748pra@gmail.com>
2025-07-09 21:24:32 +0530

    Initial commit: Smart Attendance System

---
+++ public/firebase-config.js
@@ -0,0 +1,12 @@
+// ✅ Use compat-style for browser
+const firebaseConfig = {
+  apiKey: "AIzaSyDIoFe5LBgvlyz0SKFZinmCLukilc_LBuM",
+  authDomain: "ai-surveillance-43d8f.firebaseapp.com",
+  projectId: "ai-surveillance-43d8f",
+  storageBucket: "ai-surveillance-43d8f.appspot.com",
+  messagingSenderId: "843065078278",
+  appId: "1:843065078278:web:e2faecab47bf9ea3fa8987"
+};
+
+// ✅ Initialize Firebase the compat way
+firebase.initializeApp(firebaseConfig);
